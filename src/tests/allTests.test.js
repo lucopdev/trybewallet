e9b95dc248c1d40055aaed3b7e5121e0ab5f7274
lucopdev <lucopdev@gmail.com>
2023-04-14 13:04:09 -0300

    requisito 5

--- src/tests/allTests.test.js
+++ src/tests/allTests.test.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { screen, within } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import App from '../App';
 import Wallet from '../pages/Wallet';
 import mockData from './helpers/mockData';
+
+const initialState = {
+  wallet: {
+    currencies: [...Object.keys(mockData)],
+    expenses: [{
+      id: 0,
+      value: '',
+      description: 'Hot Dog',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+      exchangeRates: mockData,
+    }],
+    editor: false,
+    idToEdit: 0,
+    allData: mockData,
+    expenseToEdit: {},
+  },
+};
 
 describe('Testa a aplicação', () => {
   it('testa se existem elementos no componente de login', () => {
@@ -43,24 +62,6 @@ describe('Testa a aplicação', () => {
   });
 
   it('Testa o componente Header', async () => {
-    const initialState = {
-      wallet: {
-        currencies: [...Object.keys(mockData)],
-        expenses: [{
-          id: 0,
-          value: '',
-          description: 'Hot Dog',
-          currency: 'USD',
-          method: 'Dinheiro',
-          tag: 'Alimentação',
-          exchangeRates: mockData,
-        }],
-        editor: false,
-        idToEdit: 0,
-        allData: mockData,
-        expenseToEdit: {},
-      },
-    };
     renderWithRouterAndRedux(<Wallet />, { initialState });
     expect(screen.getByTestId('email-field')).toBeInTheDocument();
 
@@ -74,11 +75,43 @@ describe('Testa a aplicação', () => {
     expect(totalField.innerHTML).toBe('120.73');
   });
 
-  it('Testa o form na pagina wallet', () => {
-    renderWithRouterAndRedux(<Wallet />);
-    const view = screen.getByText(/moeda:/i);
+  it('Testa o funcionamento do form na pagina wallet', () => {
+    renderWithRouterAndRedux(<Wallet />, { initialState });
+    const valueInput = screen.getByRole('spinbutton');
+    const currency = screen.getByTestId('currency-input');
+    const method = screen.getByTestId('method-input');
+    const tag = screen.getByTestId('tag-input');
+    const description = screen.getByRole('textbox');
+    const addExpense = screen.getByRole('button', { name: /adicionar despesa/i });
 
-    expect(screen.getByRole('spinbutton')).toBeInTheDocument();
-    expect(within(view).getByRole('combobox')).toBeInTheDocument();
+    userEvent.type(valueInput, '25');
+    expect(currency.value).toBe('USD');
+    expect(method.value).toBe('Dinheiro');
+    expect(tag.value).toBe('Alimentação');
+    userEvent.type(description, 'McDonalds');
+    userEvent.click(addExpense);
+
+    expect(screen.getByRole('cell', { name: /mcdonalds/i })).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: /alimentação/i })[1]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: /dinheiro/i })[0]).toBeInTheDocument();
+    expect(screen.getByRole('cell', { name: /25\.00/i })).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: /dólar americano\/real brasileiro/i })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: /4\.75/i })[0]).toBeInTheDocument();
+
+    const editBtn = screen.getAllByRole('button', {
+      name: /editar/i,
+    });
+    const deleteBtn = screen.getAllByRole('button', {
+      name: /excluir/i,
+    });
+
+    userEvent.click(editBtn[0]);
+    userEvent.type(valueInput, '34');
+    const editExpenseBtn = screen.getByRole('button', { name: /editar despesa/i });
+    userEvent.click(editExpenseBtn);
+    expect(screen.getByRole('cell', { name: /34\.00/i })).toBeInTheDocument();
+
+    userEvent.click(deleteBtn[0]);
+    expect(description.value).toBe('');
   });
 });
